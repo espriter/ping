7bd7ce4b0bc5146755ed8b978ee3da91af022f71
espriter <esprit@espriter.net>
2022-08-08 10:25:51 +0900

    init commit

---
+++ run.py
@@ -0,0 +1,29 @@
+import time
+from ping3 import ping
+import requests as res
+import json
+
+def ping_nas(request):
+    ping_list = ["sothojishin.direct.quickconnect.to"]
+
+    # URL 읽기
+    f = open("/home/espriter/github/ping/webhook_url.txt", 'r')
+    url_read = f.readline()
+    f.close()
+
+    for lst in ping_list:
+        result = ping(lst)
+        if result == False:
+            fail_msg = str('NAS Ping이 실패했어요! → ' + lst)
+            headers = {"Content-type": "application/json"}
+            data = {"text": fail_msg }
+            res_result = res.post(url_read, headers=headers, data=json.dumps(data))
+            print(res_result.status_code)
+
+        else:
+            success_msg = str('NAS Ping이 성공했어요! → ' + lst)
+            headers = {"Content-type": "application/json"}
+            data = {"text": success_msg }
+            res_result = res.post(url_read, headers=headers, data=json.dumps(data))
+            print(res_result.status_code)
+        time.sleep(2)
